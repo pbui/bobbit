49b687693fd015d80afba24c60ed0dd18bf3a44e
Peter Bui <peter.j.bui@gmail.com>
2017-05-16 19:26:30 -0400

    Add support for modules
    Include 8ball command

--- bobbit.py
+++ bobbit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2.7
 
 import getpass
+import glob
 import logging
 import os
 import re
@@ -30,9 +31,9 @@ class Bobbit(object):
              (CHANMSG_RE, self.handle_channel_message),
              (PRIVMSG_RE, self.handle_private_message),
         ]
-        self.modules     = []
 
         self.load_configuration(config_path)
+        self.load_modules()
 
     # Connect ------------------------------------------------------------------
 
@@ -122,21 +123,65 @@ class Bobbit(object):
 
     def handle_channel_message(self, nick, channel, message):
         self.logger.info('Handling Channel Message: %s | %s | %s', channel, nick, message)
-        for response in self.process_message(nick, message, channel):
+        for response in self.process_command(nick, message, channel):
             self.send_response(response, channel=channel)
 
     def handle_private_message(self, nick, message):
         self.logger.info('Handling Private Message: %s | %s', nick, message)
-        for response in self.process_message(nick, message):
+        for response in self.process_command(nick, message):
             self.send_response(response, nick=nick)
 
     # Modules ------------------------------------------------------------------
 
-    def process_message(self, nick, message, channel=None):
-        for pattern, callback in self.modules:
+    def load_modules(self):
+        self.logger.info('Importing modules from %s', self.modules_dir)
+
+        # Clear current list of modules and commands
+        self.modules  = {}
+        self.commands = []
+
+        # Iterate over modules in directory
+        for module_path in glob.glob('{}/*.py'.format(self.modules_dir)):
+            module_name = module_path[:-3].replace('/', '.').replace('..', '')
+
+            if '__' in module_name:
+                continue
+
+            # Load or re-load module
+            try:
+                self.logger.info('Loading %s', module_name)
+                if module_name in self.modules:
+                    module = self.modules[module_name]
+                    reload(module)
+                else:
+                    module = __import__(module_name, globals(), locals(), -1)
+                    self.modules[module_name] = module
+            except ImportError as e:
+                self.logger.warn('Failed to import module %s: %s', module_name, e)
+                continue
+
+            # Enable module
+            try:
+                if module.TYPE == 'command':
+                    self.commands.extend(module.register(self))
+            except AttributeError as e:
+                self.logger.info('Failed to enable module %s: %s', module_name, e)
+
+    def process_command(self, nick, message, channel=None):
+        for pattern, callback in self.commands:
             match = pattern.match(message)
             if match:
-                yield callback(nick, message, channel, **match.groupdict())
+                yield callback(self, nick, message, channel, **match.groupdict())
+    
+    # Utilities ----------------------------------------------------------------
+
+    def format_responses(self, responses, nick=None, channel=None):
+        prefix = self.nick_prefix
+        if isinstance(responses, unicode) or isinstance(responses, str):
+            yield u'{}{}: {}'.format(prefix, nick, responses) if channel else responses
+        else:
+            for response in responses:
+                self.format_responses(response, nick, channel)
 
     # Configuration ------------------------------------------------------------
 
@@ -144,26 +189,32 @@ class Bobbit(object):
         ''' Load configuration from YAML file '''
         self.work_dir    = os.environ.get('BOBBIT_DIR', os.path.expanduser('~/.config/bobbit'))
         self.config_path = config_path if config_path else os.path.join(self.work_dir, 'config.yaml')
+        self.modules_dir = os.path.join(os.path.dirname(__file__), 'modules')
+
         if os.path.exists(self.config_path):
-            self.config  = yaml.load(open(self.config_path))
+            config       = yaml.load(open(self.config_path))
             self.work_dir= os.path.dirname(self.config_path)
         else:
-            self.config  = {}
-
-        self.host        = self.config.get('host'    , 'irc.freenode.net')
-        self.port        = self.config.get('port'    , 6667)
-        self.owner       = self.config.get('owner'   , getpass.getuser())
-        self.nick        = self.config.get('nick'    , 'bobbit')
-        self.password    = self.config.get('password', '')
-        self.channels    = self.config.get('channels', [])
+            config       = {}
 
         self.logger.info('Working Directory:  %s', self.work_dir)
         self.logger.info('Configuration Path: %s', self.config_path)
+        self.logger.info('Modules Path:       %s', self.modules_dir)
+
+        self.host        = config.get('host'       , 'irc.freenode.net')
+        self.port        = config.get('port'       , 6667)
+        self.owner       = config.get('owner'      , getpass.getuser())
+        self.nick        = config.get('nick'       , 'bobbit')
+        self.nick_prefix = config.get('nick_prefix', '')
+        self.password    = config.get('password'   , '')
+        self.channels    = config.get('channels'   , [])
+
         self.logger.info('IRC Server:         %s:%d', self.host, self.port)
-        self.logger.info('IRC Owner:          %s', self.owner)
-        self.logger.info('IRC Nick:           %s', self.nick)
-        self.logger.info('IRC Password:       %s', self.password)
-        self.logger.info('IRC Channels:       %s', ', '.join(self.channels))
+        self.logger.info('IRC Owner:          %s'   , self.owner)
+        self.logger.info('IRC Nick:           %s'   , self.nick)
+        self.logger.info('IRC Nick Prefix:    %s'   , self.nick_prefix)
+        self.logger.info('IRC Password:       %s'   , self.password)
+        self.logger.info('IRC Channels:       %s'   , ', '.join(self.channels))
 
     # Run ----------------------------------------------------------------------
 
---
+++ modules/8ball.py
@@ -0,0 +1,64 @@
+import random
+import re
+
+# Meta-data --------------------------------------------------------------------
+
+NAME    = '8ball'
+ENABLE  = True
+TYPE    = 'command'
+PATTERN = re.compile('^!8ball (?P<question>.*)')
+USAGE   = ''' 8ball Module
+Usage:  !8ball <question>
+
+Example:
+
+        > !8ball Will pbui ever get tenure at Notre Dame?
+        Don't count on it
+'''
+
+# Constants --------------------------------------------------------------------
+
+RESPONSES = (
+    (   # Affirmative
+        'It is certain',
+        'It is decidedly so',
+        'Without a doubt',
+        'Yes -- definitely',
+        'You may rely on it',
+        'As I see it, yes',
+        'Most likely',
+        'Outlook good',
+        'Yes',
+        'Signs point to yes',
+    ),
+    (   # Maybe
+        'Reply hazy, try again',
+        'Ask again later',
+        'Better not tell you now',
+        'Cannot predict now',
+        'Concentrate and ask again',
+    ),
+    (   # Negative
+        'Dont count on it',
+        'My reply is no',
+        'My sources say no',
+        'Outlook not so good',
+        'Very doubtful',
+        'NOOOOO WAY',
+    )
+)
+
+# Callback ----------------------------------------------------------------------
+
+def command(bot, nick, message, channel, question=None):
+    response = random.choice(random.choice(RESPONSES))
+    return bot.format_responses(response, nick, channel)
+
+# Register ----------------------------------------------------------------------
+
+def register(bot):
+    return (
+        (PATTERN, command),
+    )
+
+# vim: set sts=4 sw=4 ts=8 expandtab ft=python:
