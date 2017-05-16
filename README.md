d17ee851a6b281dc0a6a6f8734609e5a23a7dcc8
Peter Bui <peter.j.bui@gmail.com>
2017-05-16 16:13:49 -0400

    Initial import

---
+++ README.md
@@ -0,0 +1,20 @@
+Bobbit
+======
+
+**bobbit** is a simple and modular IRC bot.  The current version uses [Tornado]
+as its core networking and event processing backend.
+
+The previous ad-hoc version can be found at
+[https://bitbucket.org/pbui/bobbit/](https://bitbucket.org/pbui/bobbit/).
+
+Requirements
+------------
+
+
+Configuration
+-------------
+
+Execution
+---------
+
+[Tornado]:  http://www.tornadoweb.org/en/stable/
---
+++ bobbit.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python2.7
+
+import getpass
+import logging
+import os
+import re
+
+import yaml
+
+import tornado.gen
+import tornado.ioloop
+import tornado.options
+import tornado.tcpclient
+
+# Regular Expressions ----------------------------------------------------------
+
+PING_RE     = re.compile('^PING (?P<payload>.*)')
+CHANMSG_RE  = re.compile(':(?P<nick>.*?)!\S+\s+?PRIVMSG\s+(?P<channel>#+[-\w]+)\s+:(?P<message>[^\n\r]+)')
+PRIVMSG_RE  = re.compile(':(?P<nick>.*?)!\S+\s+?PRIVMSG\s+[^#][^:]+:(?P<message>[^\n\r]+)')
+
+# Bobbit -----------------------------------------------------------------------
+
+class Bobbit(object):
+
+    def __init__(self, config_path=None, **kwargs):
+        self.logger      = logging.getLogger()
+        self.tcp_client  = tornado.tcpclient.TCPClient()
+        self.handlers    = [
+             (PING_RE   , self.handle_ping),
+             (CHANMSG_RE, self.handle_channel_message),
+             (PRIVMSG_RE, self.handle_private_message),
+        ]
+        self.modules     = []
+
+        self.load_configuration(config_path)
+
+    # Connect ------------------------------------------------------------------
+
+    @tornado.gen.coroutine
+    def connect(self):
+        ''' Connect to IRC server, authorize, register, and identify '''
+        self.logger.info('Connecting to %s:%d', self.host, self.port)
+        self.tcp_stream  = yield self.tcp_client.connect(self.host, self.port)
+
+        # Send connection password (e.g. Slack)
+        if self.password.startswith('CONN:'):
+            password = self.password[5:]
+            self.logger.info('Sending Connection Password: %s', password)
+            self.send('PASS {}'.format(password))
+
+        # Authorize
+        self.logger.info('Authorizing as %s', self.nick)
+        self.send('USER {} {} bobbit :{}'.format(self.nick, self.host, self.nick))
+
+        # Register
+        self.logger.info('Registering as %s', self.nick)
+        self.send('NICK {}'.format(self.nick))
+
+        # Identify
+        self.logger.info('Identifying as %s', self.nick)
+        if not self.password.startswith('CONN:'):
+            self.send_message('IDENTIFY {}'.format(self.password), nick='NickServ')
+
+        # Join channels
+        for channel in self.channels:
+            self.send('JOIN {}'.format(channel))
+
+        # Wait for next message
+        self.recv_message('')
+
+    # Send / receive messages --------------------------------------------------
+
+    @tornado.gen.coroutine
+    def send(self, message):
+        yield self.tcp_stream.write(message + '\r\n')
+
+    def send_message(self, message, channel=None, nick=None):
+        if channel:
+            receiver = channel
+        elif nick:
+            receiver = nick
+        else:
+            receiver = None
+
+        if receiver:
+            self.send('PRIVMSG {} :{}'.format(receiver, message))
+        else:
+            self.logging.warn('No channel or nick specified for: %s', message)
+
+    def send_response(self, response, nick=None, channel=None):
+        if response is None or (nick is None and channel is None):
+            return
+
+        if isinstance(response, unicode) or isinstance(response, str):
+            self.send_message(response, nick, channel)
+        else:
+            for r in response:
+                self.send_response(r, nick, channel)
+
+    def recv_message(self, message):
+        # Receive message
+        message = message.rstrip()
+        self.logger.info(message)
+
+        # Process handlers
+        for pattern, callback in self.handlers:
+            match = pattern.match(message)
+            if match:
+                try:
+                    callback(**match.groupdict())
+                except Exception as e:
+                    self.logger.exception('Unhandled exception: %s' % e)
+
+        # Wait for next message
+        self.tcp_stream.read_until('\n', self.recv_message)
+
+    # Handlers -----------------------------------------------------------------
+
+    def handle_ping(self, payload):
+        self.logger.info('Handling PING: %s'.format(payload))
+        self.send('PONG {}'.format(payload))
+
+    def handle_channel_message(self, nick, channel, message):
+        self.logger.info('Handling Channel Message: %s | %s | %s', channel, nick, message)
+        for response in self.process_message(nick, message, channel):
+            self.send_response(response, channel=channel)
+
+    def handle_private_message(self, nick, message):
+        self.logger.info('Handling Private Message: %s | %s', nick, message)
+        for response in self.process_message(nick, message):
+            self.send_response(response, nick=nick)
+
+    # Modules ------------------------------------------------------------------
+
+    def process_message(self, nick, message, channel=None):
+        for pattern, callback in self.modules:
+            match = pattern.match(message)
+            if match:
+                yield callback(nick, message, channel, **match.groupdict())
+
+    # Configuration ------------------------------------------------------------
+
+    def load_configuration(self, config_path=None):
+        ''' Load configuration from YAML file '''
+        self.work_dir    = os.environ.get('BOBBIT_DIR', os.path.expanduser('~/.config/bobbit'))
+        self.config_path = config_path if config_path else os.path.join(self.work_dir, 'config.yaml')
+        if os.path.exists(self.config_path):
+            self.config  = yaml.load(open(self.config_path))
+            self.work_dir= os.path.dirname(self.config_path)
+        else:
+            self.config  = {}
+
+        self.host        = self.config.get('host'    , 'irc.freenode.net')
+        self.port        = self.config.get('port'    , 6667)
+        self.owner       = self.config.get('owner'   , getpass.getuser())
+        self.nick        = self.config.get('nick'    , 'bobbit')
+        self.password    = self.config.get('password', '')
+        self.channels    = self.config.get('channels', [])
+
+        self.logger.info('Working Directory:  %s', self.work_dir)
+        self.logger.info('Configuration Path: %s', self.config_path)
+        self.logger.info('IRC Server:         %s:%d', self.host, self.port)
+        self.logger.info('IRC Owner:          %s', self.owner)
+        self.logger.info('IRC Nick:           %s', self.nick)
+        self.logger.info('IRC Password:       %s', self.password)
+        self.logger.info('IRC Channels:       %s', ', '.join(self.channels))
+
+    # Run ----------------------------------------------------------------------
+
+    def run(self):
+        self.connect()
+        tornado.ioloop.IOLoop.current().start()
+
+# Main Execution ---------------------------------------------------------------
+
+if __name__ == '__main__':
+    tornado.options.define('config_path', default=None,  help='Configuration path')
+    tornado.options.parse_command_line()
+
+    options = tornado.options.options.as_dict()
+    bobbit  = Bobbit(**options)
+    bobbit.run()
+
+# vim: set sts=4 sw=4 ts=8 expandtab ft=python:
