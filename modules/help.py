9eae7ec9bba25267f3bd7ad5b391be40818a7831
Peter Bui <peter.j.bui@gmail.com>
2017-05-16 20:34:37 -0400

    Add help module

---
+++ modules/help.py
@@ -0,0 +1,34 @@
+import random
+
+# Meta-data --------------------------------------------------------------------
+
+NAME    = 'help'
+ENABLE  = True
+TYPE    = 'command'
+PATTERN = '^!help (?P<module_name>.*)'
+USAGE   = '''Usage: !help [<module_name> | all]
+Either list all the modules or provide the usage message for a particular
+module.
+'''
+
+# Command -----------------------------------------------------------------------
+
+def command(bot, nick, message, channel, module_name):
+    if module_name == 'all':
+        responses = sorted([m.NAME for m in bot.modules.values()])
+        return bot.format_responses(responses, nick, channel)
+
+    for module in bot.modules.values():
+        if module.NAME == module_name:
+            return bot.format_responses(module.USAGE.splitlines(), nick, channel)
+
+    return None
+
+# Register ----------------------------------------------------------------------
+
+def register(bot):
+    return (
+        (PATTERN, command),
+    )
+
+# vim: set sts=4 sw=4 ts=8 expandtab ft=python:
