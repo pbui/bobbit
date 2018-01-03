f406670095dbe3208602098b928b4c4abaa09bee
Peter Bui <pbui@bx612.space>
2018-01-03 16:40:27 -0500

    Add common module

---
+++ modules/__common__.py
@@ -0,0 +1,20 @@
+# __common__.py: ---------------------------------------------------------------
+
+import tornado.httpclient
+
+# Short URLs -------------------------------------------------------------------
+
+def shorten_url(url):
+    request = tornado.httpclient.HTTPRequest(
+        url    = 'https://yld.me/url',
+        method = 'POST',
+        body   = url,
+    )
+    result  = tornado.httpclient.HTTPClient().fetch(request)
+
+    try:
+        return result.body.decode()
+    except AttributeError:
+        return url 
+
+# vim: set sts=4 sw=4 ts=8 expandtab ft=python: --------------------------------
