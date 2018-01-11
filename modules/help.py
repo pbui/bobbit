ca161d749adf9115409ecc4da0e270f88bca5f8c
Peter Bui <pbui@bx612.space>
2018-01-10 22:33:39 -0500

    help: support !help by itself, use notices

--- modules/help.py
+++ modules/help.py
@@ -1,13 +1,12 @@
 # help.py -----------------------------------------------------------------------
 
-import random
-
 # Meta-data ---------------------------------------------------------------------
 
 NAME    = 'help'
 ENABLE  = True
 TYPE    = 'command'
-PATTERN = '^!help (?P<module_name>.*)'
+PATTERN0= '^!help$'
+PATTERN1= '^!help (?P<module_name>.*)'
 USAGE   = '''Usage: !help [<module_name> | all]
 Either list all the modules or provide the usage message for a particular
 module.
@@ -15,14 +14,15 @@ module.
 
 # Command -----------------------------------------------------------------------
 
-def command(bot, nick, message, channel, module_name):
-    if module_name == 'all':
+def command(bot, nick, message, channel, module_name=None):
+    if module_name is None or module_name == 'all':
         responses = sorted([m.NAME for m in bot.modules.values()])
-        return bot.format_responses(responses, nick, channel)
+        bot.send_response(responses, channel, nick, notice=True)
 
     for module in bot.modules.values():
         if module.NAME == module_name:
-            return bot.format_responses(module.USAGE.splitlines(), nick, channel)
+            responses = module.USAGE.splitlines()
+            bot.send_response(responses, channel, nick, notice=True)
 
     return None
 
@@ -30,7 +30,8 @@ def command(bot, nick, message, channel, module_name):
 
 def register(bot):
     return (
-        (PATTERN, command),
+        (PATTERN0, command),
+        (PATTERN1, command),
     )
 
 # vim: set sts=4 sw=4 ts=8 expandtab ft=python: ---------------------------------
