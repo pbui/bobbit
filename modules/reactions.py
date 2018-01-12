b861f990555119cba261ceeba016dca8c439f2d4
Peter Bui <pbui@bx612.space>
2018-01-12 16:36:29 -0500

    Add reactions module

---
+++ modules/reactions.py
@@ -0,0 +1,38 @@
+# reactions.py -----------------------------------------------------------------
+
+import random
+
+# Metadata ---------------------------------------------------------------------
+
+NAME    = 'reactions'
+ENABLE  = True
+TYPE    = 'command'
+PATTERN = '^(?P<phrase>(hott|yes|omg|yay|yeah))$'
+USAGE   = '''Usage: <phrase>
+This reacts to certain words like hott or yay
+'''
+
+# Constants --------------------------------------------------------------------
+
+AGREEMENTS = (
+    'totes',
+    'ikr',
+    'indeed',
+    'u betcha',
+    'zong',
+    '\o/',
+)
+
+# Command ----------------------------------------------------------------------
+
+def command(bot, nick, message, channel, phrase=None):
+    bot.send_message(random.choice(AGREEMENTS), nick, channel)
+
+# Register ---------------------------------------------------------------------
+
+def register(bot):
+    return (
+        (PATTERN, command),
+    )
+
+# vim: set sts=4 sw=4 ts=8 expandtab ft=python: --------------------------------
