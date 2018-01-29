9210b29828a1e009becd399c8d90df1d6c225108
Peter Bui <pbui@bx612.space>
2018-01-29 09:01:55 -0500

    Add verify module

---
+++ modules/verify.py
@@ -0,0 +1,67 @@
+# verify.py --------------------------------------------------------------------
+
+import base64
+import hashlib
+import random
+import re
+import string
+import time
+
+# Metadata ---------------------------------------------------------------------
+
+NAME    = 'verify'
+ENABLE  = True
+TYPE    = 'command'
+PATTERN = '^!verify (?P<netid>[^\s]*) (?P<passcode>.*)'
+USAGE   = '''Usage: !verify netid passcode
+Given a netid and passcode, this will verify the drop.
+Example:
+    > !verify pbui 1cc8de27f54ab8f6ab92706354d483e5e5efc6d1
+'''
+
+# Constants --------------------------------------------------------------------
+
+ROT13         = str.maketrans('ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz',
+                              'NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm')
+GREETINGS     = ('hey', 'bonjour', 'hi', 'hello', 'greetings', 'hola')
+FAILURES      = (
+    'Pardonez moi?',
+    "No Puppet... No Puppet... You're the Puppet",
+    "Are your some 400 lb hacker or something?",
+    "Bzzt",
+    "How about no",
+    "Some people choose to see the ugliness in this world. The disarray. I choose to see the beauty.",
+    "I'm sorry, I'm afraid I can't do that",
+    "Segmentation Fault",
+)
+
+# Functions --------------------------------------------------------------------
+
+def cksum(s):
+    return hashlib.sha1(s.encode()).hexdigest()
+
+def rot13(s):
+    return str.translate(s, ROT13)
+
+# Command ----------------------------------------------------------------------
+
+def command(bot, nick, message, channel, netid, passcode):
+    if passcode == cksum(netid):
+        message  = rot13('{}={}'.format(netid, int(time.time()))).encode()
+        response = '{} {}! Please tell the ORACLE the following MESSAGE: {}'.format(
+            random.choice(GREETINGS).title(),
+            netid,
+            base64.b64encode(message).decode(),
+        )
+    else:
+        response = random.choice(FAILURES)
+    bot.send_response(response, nick, channel)
+
+# Register ---------------------------------------------------------------------
+
+def register(bot):
+    return (
+        (PATTERN, command),
+    )
+
+# vim: set sts=4 sw=4 ts=8 expandtab ft=python: --------------------------------
