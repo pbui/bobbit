3052fcce8eb124f6939274e300dfbce68299fb6e
Peter Bui <pbui@bx612.space>
2019-06-18 12:30:12 -0400

    clap: rename and refactor

--- modules/clap.py
+++ modules/clap.py
@@ -1,7 +1,4 @@
 # clapback.py
-# Sarah Hwang
-
-import re
 
 # Metadata
 
@@ -12,22 +9,22 @@ PATTERN = '^!clap (?P<phrase>.*)'
 USAGE   = '''Usage: !clap <phrase>
 Given a phrase, this adds a clap emoji in the whitespace.
 Example:
-    > !cb Do I look like I'm joking 
-	Do U0001F44F I U0001F44F look U0001F44F like U0001F44F I'm U0001F44F joking U0001F44F     
+    > !clap Do I look like I'm joking
+	Do U0001F44F I U0001F44F look U0001F44F like U0001F44F I'm U0001F44F joking U0001F44F
 		  '''
 # Constants
 
 # Command
 def command(bot, nick, message, channel, phrase):
-    response = phrase.replace(" ", "\U0001F44F ")
+    response = phrase.replace(" ", " \U0001F44F ")
     bot.send_response(response, nick, channel)
-	
+
 # Register
 
 def register(bot):
-return (
+    return (
 	(PATTERN, command),
-	)
+    )
 
 # vim: set sts=4 sw=4 ts=8 expandtab ft=python:
 
