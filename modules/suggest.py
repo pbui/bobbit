11f1abf3e811e51834a195defd35a66f6180653e
Peter Bui <pbui@bx612.space>
2019-06-18 12:08:54 -0400

    suggest: require senders to be verified

--- modules/suggest.py
+++ modules/suggest.py
@@ -1,5 +1,7 @@
 # suggest.py
 
+import random
+
 # Metadata
 
 NAME    = 'suggest'
@@ -14,17 +16,28 @@ Example:
 
 # Constants
 
-CHANNELS = ('cse-40175-fa18',)
+CHANNELS = ('csememes', 'gaming', 'ndlug')
+#NICKS    = ('pbui', 'U0HK21CP2', 'U6R5KVD4L', 'UF7RAN95W')
+
+REJECTS  = (
+    "You better check yourself before you wreck yourself",
+    "Doesn't look like anything to me",
+    "I'm sorry, I'm afraid I can't do that",
+    "Channel {channel} not allowed",
+    "You shall not pass!",
+    "I'll be reporting this to MD",
+    "RAMZI SAYS NO SUDO",
+)
 
 # Command
 
 def command(bot, nick, message, channel, target, suggestion):
     target = target[1:] if target.startswith('#') else target
-    if target in CHANNELS:
+    if target in CHANNELS and nick in bot.verified:
         bot.logger.info('Anonymous message from %s: %s', nick, message)
-        bot.send_response(suggestion, 'anonymous', '#' + target)
+        bot.send_response('Anonymous coward suggests: ' + suggestion, 'anonymous', '#' + target)
     else:
-        bot.send_response('Channel {} not allowed'.format(target), nick, channel)
+        bot.send_response(random.choice(REJECTS).format(channel=target), nick, channel)
 
 # Register
 
