609e1aab1eaa55b4ad8b2005f71d776adec3a131
Peter Bui <pbui@bx612.space>
2019-06-24 08:21:24 -0400

    Add new title module
    Displays title of a URL (similar to gonzobot).  Only supports standalone
    URLs.

---
+++ modules/title.py
@@ -0,0 +1,50 @@
+# title.py
+
+import re
+
+import tornado.gen
+import tornado.httpclient
+
+# Metadata
+
+NAME    = 'title'
+ENABLE  = True
+TYPE    = 'command'
+PATTERN = '^(?P<url>http.*$)'
+USAGE   = '''Usage: !g <query>
+Looks up title of URL.
+Example:
+    > http://www.insidehighered.com/quicktakes/2019/06/24/uc-santa-cruz-removes-catholic-mission-bell
+    Title: UC Santa Cruz Removes Catholic Mission Bell
+'''
+
+# Constants
+
+WHITELIST = ('##bx612', '#nd-cse')
+
+# Command
+
+@tornado.gen.coroutine
+def command(bot, nick, message, channel, url=None):
+    if channel is not None and channel not in WHITELIST:
+        return
+
+    client = tornado.httpclient.AsyncHTTPClient()
+    result = yield tornado.gen.Task(client.fetch, url)
+
+    try:
+        title    = re.findall(r'<title>(.*)</title>', result.body.decode('utf-8'))[0]
+        response = 'Title: {}'.format(bot.format_bold(title))
+    except (IndexError, ValueError) as e:
+        return
+
+    bot.send_response(response, nick, channel)
+
+# Register
+
+def register(bot):
+    return (
+        (PATTERN, command),
+    )
+
+# vim: set sts=4 sw=4 ts=8 expandtab ft=python:
