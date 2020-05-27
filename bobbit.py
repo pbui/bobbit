26c74da99564bef19b7209edbe9d52100776a911
Peter Bui <pbui@bx612.space>
2020-05-26 20:32:25 -0400

    bobbit: whitespace change
    To Test abbit

--- bobbit.py
+++ bobbit.py
@@ -170,7 +170,7 @@ class IRCClient(object):
             for channel in self.channels:
                 self.logger.info('Joining %s', channel)
                 self.send('JOIN {}'.format(channel))
-            
+
         # Uncomment if you need to REGISTER the bot
         # self.send_message('REGISTER {}'.format(self.password), nick='NickServ')
 
