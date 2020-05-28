465098564b872bbba2a46e7991578d627da6da74
Peter Bui <pbui@bx612.space>
2020-05-28 16:07:51 -0400

    clap: add utility function, add fire and tear

--- src/bobbit/modules/clap.py
+++ src/bobbit/modules/clap.py
@@ -10,21 +10,24 @@ Example:
     > !clap Do I look like I'm joking
     Do 👏 I 👏 look 👏 like 👏 I'm 👏 joking 👏
 '''
+# Utility
+
+def make_clapper(replacement):
+    return lambda *args, **kwargs: clap(*args, **kwargs, replacement=replacement)
 
 # Command
 
-async def clap(bot, message, phrase):
-    return message.with_body(phrase.replace(' ', ' 👏 '))
-
-async def crab(bot, message, phrase):
-    return message.with_body(phrase.replace(' ', ' 🦀 '))
+async def clap(bot, message, phrase, replacement='👏'):
+    return message.with_body(phrase.replace(' ', f' {replacement} '))
 
 # Register
 
 def register(bot):
     return (
         ('command', '^!clap (?P<phrase>.*)', clap),
-        ('command', '^!crab (?P<phrase>.*)', crab),
+        ('command', '^!crab (?P<phrase>.*)', make_clapper('🦀')),
+        ('command', '^!fire (?P<phrase>.*)', make_clapper('🔥')),
+        ('command', '^!tear (?P<phrase>.*)', make_clapper('💧')),
     )
 
 # vim: set sts=4 sw=4 ts=8 expandtab ft=python:
