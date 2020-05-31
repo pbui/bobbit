44ae5023689ec3bf997678c74c0a2e77d671d8db
Peter Bui <pbui@bx612.space>
2020-05-30 21:57:42 -0400

    tests: add load_tests function

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,11 @@
 ''' bobbit.tests '''
 
-from .mock import MockClient
+import os
+
+def load_tests(loader, standard_tests, pattern):
+    this_dir      = os.path.dirname(__file__)
+    package_tests = loader.discover(start_dir=this_dir, pattern='test_*.py')
+    standard_tests.addTests(package_tests)
+    return standard_tests
 
 # vim: set sts=4 sw=4 ts=8 expandtab ft=python:
