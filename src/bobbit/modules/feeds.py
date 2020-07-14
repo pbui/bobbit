4875714db3bb68f2a4acf78f13acf08558bcb858
Peter Bui <pbui@bx612.space>
2020-07-14 09:40:09 -0400

    feeds: add workround for long authorship
    The Conversation includes the author's rank and university affliation,
    which is not really necessary.  Simply pick the first author before the
    comma and be done with it.

--- src/bobbit/modules/feeds.py
+++ src/bobbit/modules/feeds.py
@@ -38,7 +38,7 @@ async def process_feed(http_client, feed, cache):
     for entry in feedparser.parse(feed_content)['entries']:
         link   = entry.get('link', '')
         title  = strip_html(entry.get('title', ''))
-        author = entry.get('author', 'Unknown')
+        author = entry.get('author', 'Unknown').split(',')[0]   # Workaround long authorship in The Conversation
         key    = link.encode('ascii','ignore')
 
         # If link starts with //, replace with https:// (workaround for the Week bug)
