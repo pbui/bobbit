5bae953d42c2a297448406ca40447843e702b1b3
Nuolong <nmcshea@nd.edu>
2020-10-05 17:02:43 -0400

    changes addressed

--- src/bobbit/modules/tell.py
+++ src/bobbit/modules/tell.py
@@ -1,5 +1,7 @@
 # tell.py
 
+from collections import defaultdict
+
 # Metadata
 NAME    = 'tell'
 PATTERN = r''
@@ -9,20 +11,17 @@ This queues a message to send to a user the next time they are active (ie.
 the next time they send a message).
 '''
 
-MAILBOX = {}
+MAILBOX = defaultdict(list)
 
 # Command
 
 async def tell(bot, message):
     if message.body.startswith("!tell"):
-        tell_message = message.body[5:].split()
-        if tell_message[0] in MAILBOX:
-            MAILBOX[tell_message[0]].append(tell_message[0] + ": " + ' '.join(tell_message[1:]))
-        else:
-            MAILBOX[tell_message[0]] = [tell_message[0] + ": " + ' '.join(tell_message[1:])]
+        target, *body = message.body[5:].split()
+        MAILBOX[target].append(message.nick + ": " + ' '.join(body))
 
     if message.nick in MAILBOX:
-        to_tell = [message.with_body(f'Tell from {msg}') for msg in MAILBOX[message.nick]]
+        to_tell = [message.copy(body=f'Tell from {msg}', channel=None, notice=True) for msg in MAILBOX[message.nick]]
         del MAILBOX[message.nick]
         return to_tell
 
