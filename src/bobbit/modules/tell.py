335d6407cf901af0f029bc560275d8324e7d1ffe
Nolan McShea <nmcshea@nd.edu>
2020-10-05 17:31:17 -0400

    tell module reworked (previously mail module) (#21)
    Add `tell` module
    
    This queues to send to users when they return from inactivity (ie. when they send another message).  The messages should be delivered as **notices**.

---
+++ src/bobbit/modules/tell.py
@@ -0,0 +1,34 @@
+# tell.py
+
+from collections import defaultdict
+
+# Metadata
+NAME    = 'tell'
+PATTERN = r''
+ENABLE  = True
+USAGE   = '''Usage: !tell <user> <message>
+This queues a message to send to a user the next time they are active (ie.
+the next time they send a message).
+'''
+
+MAILBOX = defaultdict(list)
+
+# Command
+
+async def tell(bot, message):
+    if message.body.startswith("!tell"):
+        target, *body = message.body[5:].split()
+        MAILBOX[target].append(message.nick + ": " + ' '.join(body))
+
+    if message.nick in MAILBOX:
+        message.channel = None # Make Message a PM/Notice
+        to_tell = [message.copy(body=f'Message from {msg}', notice=True) for msg in MAILBOX[message.nick]]
+        del MAILBOX[message.nick]
+        return to_tell
+
+# Register
+
+def register(bot):
+    return (
+        ('command', PATTERN, tell),
+    )
