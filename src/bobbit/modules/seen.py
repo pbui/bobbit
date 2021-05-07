52a49aab0585bcc594fd4ba91db2f89a0c0b515f
Peter Bui <pbui@bx612.space>
2021-05-07 00:05:58 -0400

    seen: list five oldest seen nicks if no argument is passed to command

--- src/bobbit/modules/seen.py
+++ src/bobbit/modules/seen.py
@@ -5,7 +5,7 @@ import time
 # Metadata
 
 NAME    = 'seen'
-PATTERN = r'^!seen\s+(?P<nick>.*)$'
+PATTERN = r'^!seen\s*(?P<nick>[^\s]*)$'
 ENABLE  = True
 USAGE   = '''Usage: !seen <nick>
 This reports the last time a user was seen (ie. last time they sent a
@@ -33,14 +33,21 @@ def elapsed_time(s):
 # Command
 
 async def seen(bot, message, nick):
-    if nick not in bot.users:
-        return message.with_body(f'Unknown nick: {nick}')
+    if nick:
+        if nick not in bot.users:
+            return message.with_body(f'Unknown user: {nick}')
 
-    last_seen = bot.users[nick].get('last_seen')
-    if not last_seen:
-        return message.with_body(f'{nick} has not been seen yet')
+        last_seen = bot.users[nick].get('last_seen')
+        if not last_seen:
+            return message.with_body(f'{nick} has not been seen yet')
 
-    return message.with_body(f'{nick} was last seen {elapsed_time(last_seen)}')
+        return message.with_body(f'{nick} was last seen {elapsed_time(last_seen)}')
+    else:
+        users = [nick for nick in bot.users if 'last_seen' in bot.users[nick] and message.channel in bot.users[nick].get('channels', [])]
+        nicks = sorted(users, key=lambda nick: bot.users[nick]['last_seen'])[:5]
+        return [
+            message.with_body(f'{nick} was last seen {elapsed_time(bot.users[nick]["last_seen"])}') for nick in nicks
+        ]
 
 # Register
 
