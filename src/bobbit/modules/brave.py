c0b0293c828ee478ec9b8baf7b597998f821a187
Peter Bui <pbui@bx612.space>
2021-06-22 13:44:35 -0400

    Add brave module
    Support for new brave search engine.

---
+++ src/bobbit/modules/brave.py
@@ -0,0 +1,54 @@
+# brave.py
+
+import html
+import json
+import logging
+import re
+
+from bobbit.utils import shorten_url, strip_html
+
+# Metadata
+
+NAME    = 'brave'
+ENABLE  = True
+PATTERN = '^!brave (?P<query>.*$)'
+USAGE   = '''Usage: !brave <query>
+Given a search query, this returns the first result from Brave.
+Example:
+    > !brave who likes short shorts?
+'''
+
+# Constants
+
+BRAVE_URL = 'https://search.brave.com/search'
+BRAVE_RE  = r"data='(.*score.*)'"
+
+# Command
+
+async def brave(bot, message, query=None):
+    async with bot.http_client.get(BRAVE_URL, params={'q': query}) as response:
+        try:
+            text     = await response.text()
+            data     = json.loads(html.unescape(re.findall(BRAVE_RE, text)[0]))['search']['web']['results'][0]
+            url      = await shorten_url(bot.http_client, data['url'])
+            title    = strip_html(html.unescape(data['title']))
+            response = bot.client.format_text(
+                '{color}{green}Brave{color}: ' +
+                '{bold}{title}{bold} @ {color}{blue}{url}{color}',
+                title = title,
+                url   = url
+            )
+        except (IndexError, ValueError) as e:
+            logging.warning(e)
+            response = 'No results'
+
+        return message.with_body(response)
+
+# Register
+
+def register(bot):
+    return (
+        ('command', PATTERN, brave),
+    )
+
+# vim: set sts=4 sw=4 ts=8 expandtab ft=python:
