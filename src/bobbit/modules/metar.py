65454029593199b537e6f3955a9e87a9e96314f6
Zephan M. Enciso <56936831+zmenciso@users.noreply.github.com>
2021-10-29 11:17:46 -0400

    Metar (#48)
    * Initial import
    
    * METARv1 complete
    
    * METAR: Fix regex

---
+++ src/bobbit/modules/metar.py
@@ -0,0 +1,66 @@
+# metar.py
+
+import logging
+import re
+
+# Metadata
+
+NAME    = 'metar'
+ENABLE  = True
+USAGE   = '''Usage: !metar <id> <date>
+Given a station ID, produce the METeorological Aerodrome Report. 
+Date is in format yyyymmddhhnn
+
+Examples:
+    > !metar                    # Default location
+    > !metar kphx               # Phoenix Sky Harbor
+    > !metar ksbn 202110271235  # South Bend 2021-10-27 at 12:35
+'''
+
+# Thanks for the regex help, pbui
+PATTERN = r'^!metar\s*(?P<ids>[a-zA-Z]+)*\s*(?P<date>[0-9]+)*$'
+
+
+# Constants
+
+DEFAULT_ID = 'ksbn'
+METAR_URL_BASE = 'https://aviationweather.gov/metar/data?'
+METAR_URL_EXT = 'format=raw&hours=0&taf=off&layout=off'
+EXTRACT = r'<code>(.*)</code>'
+
+# Functions
+
+async def get_metar_data(bot, ids, date):
+    url = METAR_URL_BASE
+
+    if ids:
+        url = url + f'ids={ids}&'
+    else:
+        url = url + f'ids={DEFAULT_ID}&'
+
+    url = url + METAR_URL_EXT
+
+    if date:
+        url = url + f'&date={date}'
+
+    async with bot.http_client.get(url) as response:
+        return await response.text()
+
+async def metar(bot, message, ids=None, date=None):
+    data    = await get_metar_data(bot, ids, date)
+
+    metar = re.findall(EXTRACT, data)
+
+    if '<strong>No METAR found' in data or not metar:
+        return message.with_body('No results')
+
+    return message.with_body(metar[0])
+
+# Register
+
+def register(bot):
+    return (
+        ('command', PATTERN, metar),
+    )
+
+# vim: set sts=4 sw=4 ts=8 expandtab ft=python:
