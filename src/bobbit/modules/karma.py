b71c0321c1605642ebf0e37f88dd5d9a575a303c
John Sullivan <jpsullivan2019@gmail.com>
2022-05-19 02:27:44 -0700

    karma: changed karma module to accept display, add, and subtract options

--- src/bobbit/modules/karma.py
+++ src/bobbit/modules/karma.py
@@ -4,44 +4,96 @@
 
 NAME    = 'karma'
 ENABLE  = True
-PATTERN = r'(?P<nick>[^\s]+)(?P<modifier>[+-]{2})$'
-USAGE   = '''Usage: nick++ or nick--
-This adds or removes karma from specified nick (or word).
+PATTERN = r'^!karma\s+(?P<op>-d|-a|-s)\s+(?P<target>(?:\w+\s?){1,5})$|(?P<shorttarget>[^\s]+)(?P<shortop>[+-]{2})$' # captures '!karma <-d | -a | -s> <1-5 words>' and '<word/nick><++/-->'
+USAGE   = '''Usage: <nick/word><++/--> OR !karma [-d | -a | -s] <nick | word/phrase>
+This displays, adds, or subtracts karma for the specified nick or word/phrase. Phrases may be up to 5 words.
 Example:
-    > AndroidKitKat++
-    AndroidKitKat now has 69 karma.
+    > macos++
+    macos now has 1 karma.
 
     > sussy--
-    sussy now has -420 karma
-'''
+    sussy now has -13 karma.
 
-# TODO: add !karma <nick|word> to display current karma
+    > !karma -d lug
+    'lug' now has 420 karma.
+
+    > !karma -a AndroidKitKat
+    'AndroidKitKat' now has 69 karma.
+
+    > !karma -s wow systems really sucks
+    'wow systems really sucks' now has -69420 karma.
+'''
 
 # Command
 
-async def karma(bot, message, nick, modifier):
-    # Prevent users from modifying their own karma
-    if nick == message.nick:
-        return
+async def karma(bot, message, op, target, shortop, shorttarget):
+    # Process short op and target
+    if shortop and shorttarget:
 
-    # Compute karma
-    dkarma = 1 if modifier == '++' else -1
+        # convert short to long opt
+        if shortop == '++':
+            op = '-a'
+        else:
+            op = '-s'
 
-    if nick in bot.users:   # Nick
-        karma = bot.users[nick]['karma'] = bot.users[nick].get('karma', 0) + dkarma
-    else:                   # Word (hack using @karam@ user)
-        user  = '@karma@'
+        # move target
+        target = shorttarget
 
-        if user not in bot.users:
-            bot.users[user] = {}
+    # strip target (in case of extra whitespace, etc.)
+    target = target.strip()
 
-        karma = bot.users[user][nick] = bot.users[user].get(nick, 0) + dkarma
+    # Process display operation
+    if op == '-d':
 
-    return message.with_body(
-        bot.client.format_text(
-            '{bold}' + nick + '{bold} now has {color}' + ('{green}' if karma >= 0 else '{red}') + str(karma) + ' karma {color}'
+        # target is a nick and has karma
+        if target in bot.users and bot.users[target]['karma']:
+            karma = bot.users[target]['karma']
+
+        # target is not a nick and has karma (see comment below for explanation of @karma@)
+        elif target in bot.users['@karma@'] and bot.users['@karma@'][target]:
+            karma = bot.users[target]['@karma@'][target]
+
+        else: # target has no karma
+            return message.with_body(
+                bot.client.format_text(
+                    '{bold}' + target + '{bold} has no karma.'
+                )
+            )
+
+        # return karma if target has any
+        return message.with_body(
+                bot.client.format_text(
+                    '{bold}' + target + '{bold} has {color}' + ('{green}' if karma >= 0 else '{red}') + str(karma) + ' karma{color}.'
+                )
+            )
+
+    else: # Process add and subtract karma operations
+
+        # Prevent users from modifying their own karma
+        # Or the karma of a phrase mentioning them
+        if message.nick in target:
+            return
+
+        # Compute karma
+        dkarma = 1 if op == '-a' else -1
+
+        if target in bot.users: # Nick
+            karma = bot.users[target]['karma'] = bot.users[target].get('karma', 0) + dkarma
+        else:
+            # Word/Phrase (hack using @karam@ user); this is the dummy karma
+            # user, which has an extra dict of (word : karma) mappings
+            user  = '@karma@'
+
+            if user not in bot.users:
+                bot.users[user] = {}
+
+            karma = bot.users[user][target] = bot.users[user].get(target, 0) + dkarma
+
+        return message.with_body(
+            bot.client.format_text(
+                '{bold}' + target + '{bold} now has {color}' + ('{green}' if karma >= 0 else '{red}') + str(karma) + ' karma{color}.'
+            )
         )
-    )
 
 # Register
 
