082f932a44168c0445c79cb0be7431a19bf06c75
jporubci <97643734+jporubci@users.noreply.github.com>
2022-10-06 20:24:20 -0400

    More bully (#65)
    * Added some lines
    
    lmk if they're too harsh lol- sorry
    
    * added bully line

--- src/bobbit/modules/bully.py
+++ src/bobbit/modules/bully.py
@@ -16,6 +16,15 @@ This bullies the user identified by nick.
 BULLY_PHRASES = [
     '\'s day has been ruined by your message,',
     ' wants to return to monke, but not if you\'re coming, too,',
+    ' knows how much of a duck-banging degenerate you are,',
+    ' hopes you order pizza from Modern Market, but then you realize you have an interview to go to that you\'re about to be late to, so you frantically rush to it before realizing it\'s over Zoom, so you pull out your laptop and search through your email, but can\'t find the link, before finally discovering it 2 whole minutes later, making you late to your interview, which you fail by the way, after which you remember you ordered pizza which, even though cold, would still be enough to lift your spirits up a little, except you find it was taken by someone else, Modern Market has closed, and you are left with nothing but dread, disgust, and misery,',
+    ' believes you\'re too incompetent to know that you\'re being bullied,',
+    ' doesn\'t care about your race, sex, or age... or anything about you really,',
+    ' has more maidens than you,',
+    '\'s faith in society has plummeted since meeting you,',
+    ' think you\'re about as good as the dining hall food,',
+    ' gives eMoTiOnAL dAmAgE to',
+    ' has a confession to make. You\'re ugly,',
     ' thinks you were probably the pilot of Ever Given when it clogged the Suez Canal,'
 ]
 
