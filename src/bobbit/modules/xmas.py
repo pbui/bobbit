0e69f7fec95aadb45d55704f8234602a6387abc5
annaklempay <97312978+annaklempay@users.noreply.github.com>
2022-10-17 14:49:51 -0500

    xmas (#66)
    * xmas
    
    * xmas: fix spacing
    
    Co-authored-by: Anna Klempay <annaklempay@Annas-MacBook-Pro.local>

---
+++ src/bobbit/modules/xmas.py
@@ -0,0 +1,30 @@
+# xmas.py
+
+import datetime
+
+# Metadata
+
+NAME    = 'xmas'
+ENABLE  = True
+PATTERN = r'^!xmas\s*$'
+USAGE   = '''Usage: !xmas
+This prints out the number of days until Christmas.
+'''
+
+# Command
+
+async def xmas(bot, message):
+
+    now = datetime.datetime.now()
+    delta = datetime.datetime(year=now.year+1, month=12, day=25) - datetime.datetime(year=now.year, month=now.month, day=now.day)
+    daysleft = delta.days % 365
+    return message.with_body(f'{daysleft} days until Christmas')
+
+# Register
+
+def register(bot):
+    return (
+        ('command', PATTERN, xmas),
+    )
+
+# vim: set sts=4 sw=4 ts=8 expandtab ft=python:
