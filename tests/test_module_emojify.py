b54e6ff199db41356fbc81892c1262908c9ce174
Evan Day <evanday19@gmail.com>
2023-02-23 23:35:27 -0500

    emojify: added test for add_emojis

--- tests/test_module_emojify.py
+++ tests/test_module_emojify.py
@@ -5,7 +5,7 @@ import sys
 
 sys.path.insert(0, 'src')
 
-from   src.bobbit.modules.emojify import get_emoji_match
+from src.bobbit.modules import emojify
 
 # Test Cases
 
@@ -40,12 +40,24 @@ class EmojifyTestCase(unittest.IsolatedAsyncioTestCase):
         }
 
         for word, expected in cases.items():
-            res = await get_emoji_match(self.dummy_table, word)
+            res = emojify.get_emoji_match(self.dummy_table, word)
 
             if expected is None:
                 self.assertIsNone(res)
             else:
-                self.assertIn(res, expected)    
+                self.assertIn(res, expected)
+
+    async def test_01_add_emojis(self):
+        cases = {
+            'ball so hard': ['ball 🏀 so hard'],
+            'nothing to see here': ['nothing to see here'],
+            'raining cat and dog': (f'raining cat {c} and dog {d}'
+                                        for c in self.dummy_table['cat']
+                                        for d in self.dummy_table['dog'])
+        } 
+
+        for text, expected in cases.items():
+            self.assertIn(emojify.add_emojis(text, self.dummy_table), expected)
 
 if __name__ == '__main__':
     unittest.main()
