410acb4457eba67c7cf1a8400f5ff7048d37341d
Peter Bui <pbui@bx612.space>
2023-06-07 10:23:55 -0400

    Add reminders module

---
+++ src/bobbit/modules/reminders.py
@@ -0,0 +1,96 @@
+# reminders.py
+
+''' Reminder system '''
+
+import dbm
+import json
+import time
+
+from bobbit.message import Message
+from bobbit.utils   import elapsed_time
+
+# Metadata
+
+NAME    = 'reminders'
+ENABLE  = True
+PATTERN = r'^!remind\s+(?P<timespec>[^\s]+)\s*(?P<body>.+)$'
+USAGE   = '''Usage: !remind timespec reminder
+!remind 5h email apple lady
+'''
+TIMEOUT = 60    # Every minute
+
+# Constants
+
+UNITS = {
+    's': 1,
+    'm': 60,
+    'h': 60*60,
+    'd': 24*60*60,
+}
+
+# Functions
+
+def parse_timespec(timespec):
+    ''' Parse time specification with the following format:
+
+        N[s|m|h|d]
+
+    TODO: Add support for exact time:
+
+        @12:34
+    '''
+    return float(timespec[:-1]) * UNITS.get(timespec[-1].lower(), 0)
+
+# Command
+
+async def reminders_command(bot, message, timespec, body):
+    reminders_path = bot.config.get_config_path('reminders.cache')
+
+    with dbm.open(reminders_path, 'c') as reminders:
+        delta = parse_timespec(timespec)
+        if not delta:
+            return message.with_body(f'Invalid time specification: {timespec}')
+
+        nick = body.split()[0]
+        if nick in bot.users:               # XXX: can't use nick as first word in body
+            nick, body = body.split(' ', 1)
+        else:
+            nick = message.nick
+
+        current = time.time()
+        timeout = str(current + delta)      # XXX: possible but unlikely collision
+
+        reminders[timeout] = json.dumps({
+            'nick': nick,
+            'body': body,
+            'time': current,
+        })
+
+# Timer
+
+async def reminders_timer(bot):
+    reminders_path = bot.config.get_config_path('reminders.cache')
+    current_time   = time.time()
+
+    with dbm.open(reminders_path, 'c') as reminders:
+        for timeout in reminders.keys():
+            reminder = json.loads(reminders[timeout])
+            elapsed  = elapsed_time(current_time, reminder['time'])
+            if float(timeout) < current_time:
+                await bot.outgoing.put(Message(
+                    nick    = reminder['nick'],
+                    channel = None,
+                    body    = f'Reminder from {elapsed} ago: {reminder["body"]}',
+                    notice  = True,
+                ))
+                del reminders[timeout]
+
+# Register
+
+def register(bot):
+    return (
+        ('command', PATTERN, reminders_command),
+        ('timer'  , TIMEOUT, reminders_timer),
+    )
+
+# vim: set sts=4 sw=4 ts=8 expandtab ft=python:
