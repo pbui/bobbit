b8a1469105bf660a47dafb7690a809993a6da558
Peter Bui <pbui@bx612.space>
2023-08-08 12:00:00 -0400

    tell: use mailbox list in bot.users, allow for multiple targets
    - This allows for messages to persist (before if bobbit restarted, they
      were last b/c they were stored in memory only)
    
    - This allows for !tell a,b,c message, which will queue up messages to
      users a, b, c

--- src/bobbit/modules/tell.py
+++ src/bobbit/modules/tell.py
@@ -1,8 +1,7 @@
 # tell.py
 
-from collections import defaultdict
-
 # Metadata
+
 NAME    = 'tell'
 PATTERN = r''
 ENABLE  = True
@@ -11,19 +10,32 @@ This queues a message to send to a user the next time they are active (ie.
 the next time they send a message).
 '''
 
-MAILBOX = defaultdict(list)
-
 # Command
 
 async def tell(bot, message):
+    # Saving
     if message.body.startswith("!tell"):
-        target, *body = message.body[5:].split()
-        MAILBOX[target].append(message.nick + ": " + ' '.join(body))
+        targets, body = message.body[5:].split(maxsplit=1)
 
-    if message.nick in MAILBOX:
+        for target in targets.split(','):
+            if target not in bot.users:
+                bot.users[target] = {}
+
+            saved_message = message.nick + ": " + body
+
+            try:
+                bot.users[target]['mailbox'].append(saved_message)
+            except KeyError:
+                bot.users[target]['mailbox'] = [saved_message]
+
+    # Sending
+    if message.nick in bot.users and bot.users[message.nick].get('mailbox', []):
         message.channel = None # Make Message a PM/Notice
-        to_tell = [message.copy(body=f'Message from {msg}', notice=True) for msg in MAILBOX[message.nick]]
-        del MAILBOX[message.nick]
+        to_tell = [
+            message.copy(body=f'Message from {msg}', notice=True)
+            for msg in bot.users[message.nick]['mailbox']
+        ]
+        del bot.users[message.nick]['mailbox']
         return to_tell
 
 # Register
