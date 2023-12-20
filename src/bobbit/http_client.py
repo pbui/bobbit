ffbe00877acbf7788005b221f78b8a4219fdb118
Peter Bui <pbui@bx612.space>
2023-12-20 07:55:20 -0500

    http_client: update user-agent

--- src/bobbit/http_client.py
+++ src/bobbit/http_client.py
@@ -8,7 +8,7 @@ class HTTPClient(aiohttp.ClientSession):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, headers={
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/112.0',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:121.0) Gecko/20100101 Firefox/121.0',
             'Connection': 'keep-alive',
         }, **kwargs)
 
