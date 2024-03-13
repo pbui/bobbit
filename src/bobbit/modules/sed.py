df508e2c1c0f5f23585a3c2aed7969a3ed20ad46
Colin McKechney <colin@mckechney.com>
2024-03-12 18:34:30 -0700

    sed now respects g

--- src/bobbit/modules/sed.py
+++ src/bobbit/modules/sed.py
@@ -3,10 +3,9 @@
 import re
 
 # Metadata
-
 NAME    = 'sed'
 ENABLE  = True
-PATTERN = '^s/(?P<pattern>[^/]+)/(?P<replacement>[^/]*)[/]*$'
+PATTERN = '^s/(?P<pattern>[^/]+)/(?P<replacement>[^/]*)[/]?(?P<repeat>[g])?$'
 USAGE   = '''Usage: s/pattern/replacement/
 This searches the channel's history for the most recent line that has the
 pattern and then performs the replacement.
@@ -14,13 +13,17 @@ pattern and then performs the replacement.
 
 # Command
 
-async def sed(bot, message, pattern, replacement):
+async def sed(bot, message, pattern, replacement, repeat=None):
     replacement = bot.client.format_text('{bold}{}{bold}', replacement)
     for original in bot.history.search(message.channel, pattern=pattern, limit=5, reverse=True):
         if re.search(PATTERN, original.body):
             continue
 
-        replaced = re.sub(pattern, replacement, original.body)
+        if repeat == None:
+            replaced = re.sub(pattern, replacement, original.body, count=1)
+        else:
+            replaced = re.sub(pattern, replacement, original.body,)
+
         return original.copy(body=replaced)
 
 # Register
