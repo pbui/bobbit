a0e2ec68d33008138aaafdcaccb27f7225ad4655
John Sullivan <26159938+antithalian@users.noreply.github.com>
2025-09-26 19:34:02 -0400

    adds some new messages to duckhunt

--- src/bobbit/modules/duckhunt.py
+++ src/bobbit/modules/duckhunt.py
@@ -36,13 +36,19 @@ KILL_MISSES = (
     'WHOOSH! You missed the duck completely!',
     'Your gun jammed!',
     'Better luck next time.',
-    'WTF!? Who are you, Dick Cheney?'
+    'WTF!? Who are you, Dick Cheney?',
+    'You aimed... at the sky. The duck is unimpressed.',
+    'The duck called in air support. You missed in the chaos.',
+    'You fired, but only confetti came out. Party time for the duck!'
 )
 SAVE_MISSES = (
     "The duck didn't want to be friends, maybe next time.",
     "Well this is awkward, the duck needs to think about it.",
     "The duck said no, maybe bribe it with some pizza? Ducks love pizza don't they?",
-    "Who knew ducks could be so picky?"
+    "Who knew ducks could be so picky?",
+    "You offered friendship, but the duck prefers solitude.",
+    "The duck is on a strict 'no new friends' diet.",
+    "The duck is busy updating its social media. Try again later."
 )
 COOLDOWN_AMOUNT = 7.0
 COOLDOWN_MESSAGE = f'You can try again in {COOLDOWN_AMOUNT} seconds.'
